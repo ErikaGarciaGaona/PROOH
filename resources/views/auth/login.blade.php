eddb9969503c913cc88cbb2965df49944f412117
unknown <erikagag99@gmail.com>
2023-08-04 10:34:42 -0600

    version 1 login

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,84 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <title>PROOH</title>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <!-- Bootstrap CSS v5.2.1 -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+
+</head>
+
+<body>
+<section class="h-100 gradient-form" style="background-color: #eee;">
+  <div class="container py-5 h-100">
+    <div class="row d-flex justify-content-center align-items-center h-100">
+      <div class="col-xl-10">
+        <div class="card rounded-3 text-black">
+          <div class="row g-0">
+            <div class="col-lg-6">
+              <div class="card-body p-md-5 mx-md-4">
+
+                <div class="text-center">
+                  <img src="https://prooh.mx/wp-content/uploads/2022/03/PROOH-logo.png"
+                  style="width: 185px;" alt="logo">
+                  <h3 class="mt-1 mb-5 pb-1">Iniciar sesión</h3>
+                </div>
+
+                <form>
+                  <p></p>
+
+                  <div class="form-outline mb-4">
+                    <input type="email" id="form2Example11" class="form-control"
+                      placeholder="Correo electrónico" />
+                    <label class="form-label" for="form2Example11"></label>
+                  </div>
+
+                  <div class="form-outline mb-4">
+                    <input type="Contraseña" id="form2Example22" class="form-control"
+                    placeholder="Contraseña"  />
+                    <label class="form-label" for="form2Example22"></label>
+                  </div>
+
+                  <div class="text-center pt-1 mb-5 pb-1">
+                    <button class="btn btn-primary btn-block fa-lg gradient-custom-2 mb-3" type="button">Iniciar Sesión</button>
+                    <a class="text-muted" href="#!">¿Olvidaste tu contraseña?</a>
+                  </div>
+
+                  <div class="d-flex align-items-center justify-content-center pb-4">
+                    <p class="mb-0 me-2">¿No tienes cuenta?</p>
+                    <button type="button" class="btn btn-outline-danger">Crear cuenta</button>
+                  </div>
+
+                </form>
+
+              </div>
+            </div>
+            <div class="col-lg-6 d-flex align-items-center gradient-custom-2">
+              <div class="text-white px-3 py-4 p-md-5 mx-md-4">
+                <h4 class="mb-4">Información de ingreso</h4>
+                <p class="small mb-0">Pulsar el icono del correo para solicitar una cuenta
+                    para ingersar a la página.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+  <!-- Bootstrap JavaScript Libraries -->
+  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"
+    integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous">
+  </script>
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.min.js"
+    integrity="sha384-7VPbUDkoPSGFnVtYi0QogXtr74QeVeeIs99Qfg5YCF+TidwNdjvaKZX19NZ/e6oz" crossorigin="anonymous">
+  </script>
+</body>
+
+</html>
